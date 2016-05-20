eb845467ee53d5638915137f3cd9a2970f68403f
Jordan Facibene <jordan.facibene13@ise.com>
2016-05-20 13:41:11 -0400

    initial commit

---
+++ autostager.py
@@ -0,0 +1,56 @@
+import os
+
+def access_token():
+    return os.environ['access_token']
+
+
+def alphafy(a_string):
+    pass
+
+
+def stage_upstream():
+    pass
+
+
+def process_pull(pr):
+    pass
+
+
+def comment_or_close(p, pr, add_comment = True):
+    pass
+
+
+def authenticated_url(s):
+    pass
+
+
+def base_dir():
+    pass
+
+
+def clone_dir(pr):
+    alphafy(pr.head.label)
+
+
+def staging_dir(pr):
+    pass
+
+
+def repo_slug():
+    pass
+
+
+def client():
+    pass
+
+
+def timeout_seconds():
+    pass
+
+
+def safe_dirs():
+    pass
+
+
+def run():
+    pass
