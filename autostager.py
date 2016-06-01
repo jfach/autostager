3e22ae653760e3ecd4736c2cbc4eee5082acd07e
Jordan Facibene <jordan.facibene13@ise.com>
2016-06-01 10:57:14 -0400

    Added .editorconfig, timeout.py, git_timeout.py, updated pull_request.py and autostager.py

--- autostager.py
+++ autostager.py
@@ -1,56 +1,102 @@
 import os
+import shutil
+import github3
+import logger
+import pull_request
 
-def access_token():
-    return os.environ['access_token']
+class Autostager():
 
+    def __init__(self):
+        slug = self.repo_slug().split('/')
+        self.owner = slug[0]
+        self.repo = slug[1]
+        self.access_token = os.environ.get('access_token')
 
-def alphafy(a_string):
-    pass
+    def access_token(self):
+        return os.environ['access_token']
 
+    def alphafy(self, a_string):
+        pass
 
-def stage_upstream():
-    pass
-
-
-def process_pull(pr):
-    pass
-
-
-def comment_or_close(p, pr, add_comment = True):
-    pass
-
-
-def authenticated_url(s):
-    pass
-
-
-def base_dir():
-    pass
-
-
-def clone_dir(pr):
-    alphafy(pr.head.label)
-
-
-def staging_dir(pr):
-    pass
-
-
-def repo_slug():
-    pass
-
-
-def client():
-    pass
+    def stage_upstream(self):
+        default_branch = client.repository(self.owner, self.repo).default_branch
+        logger.log("===> begin {0}".format(default_branch))
+        p = pull_request.PullRequest(
+            default_branch,
+            self.authenticated_url("https://github.com/{0}".format(self.repo_slug())),
+            self.base_dir(),
+            default_branch,
+            self.authenticated_url("https://github.com/{0}".format(self.repo_slug()))
+        )
+        if not p.staged():
+            p.clone()
+        p.fetch()
+        if p.rebase():
+            return
+        self.client.repository(self.owner, self.repo).create_issue(
+            "Failed to fast-forward {0} branch".format(default_branch),
+            ":bangbang: This probably means somebody force-pushed to the branch."
+        )
+        pass
 
 
-def timeout_seconds():
-    pass
+
+    def process_pull(self, pr):
+        logger.log("===> {0} {1}".format(pr.number, self.staging_dir(pr)))
 
 
-def safe_dirs():
-    pass
+    def comment_or_close(self, p, pr, add_comment = True):
+        pass
 
 
-def run():
-    pass
+    def authenticated_url(s):
+        pass
+
+
+    def base_dir():
+        return os.environ.get('base_dir') or '/opt/puppet/environments'
+
+
+    def clone_dir(pr):
+        alphafy(pr.head.label)
+
+
+    def staging_dir(self, pr):
+        os.path.join(self.base_dir(), self.clone_dir(pr))
+
+
+    def repo_slug():
+        return os.environ.get('repo_slug') # handle None return value if key does not exist?
+
+
+    def client():
+        return github3.login(token=access_token())
+
+
+    def timeout_seconds():
+        result = 120
+        if os.environ.get('timeout'):
+            result = int(os.environ['timeout'])
+            assert result > 0, "timeout must be greater than zero seconds"
+        return result
+
+
+    def safe_dirs():
+        return ['.', '..', 'production']
+
+    def run(self):
+        self.client()
+        self.stage_upstream()
+        prs = client.repository(self.owner, self.repo).pull_requests()
+        new_clones = [self.clone_dir(pr) for pr in prs]
+        if os.path.exists(self.base_dir()):
+            discard_dirs = set(os.listdir(self.base_dir())) - set(self.safe_dirs()) - set(new_clones)
+            discard_dirs = list(discard_dirs)
+            discard_dirs = [os.path.join(self.base_dir(), d) for d in discard_dirs]
+            for discard_dir in discard_dirs:
+                logger.log("===> Unstage {0} since PR is closed.".format(discard_dir))
+                shutil.rmtree(discard_dir) 
+        with timeout(self.timeout_seconds()):
+            for pr in prs:
+                self.process_pull(pr)
+        pass
