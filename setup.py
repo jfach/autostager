2d8a6bc5b569d9838399c48e7e7705d8ed960f91
Jordan Facibene <jordan.facibene13@stjohns.edu>
2016-06-07 09:40:36 -0400

    change pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'autostager',
   packages = ['autostager'],
-  version = '0.1.23',
+  version = '0.1.24',
   description = 'Stage a directory based on Github pull request (e.g., dynamic puppet environments)',
   author = 'Jordan Facibene',
   author_email = 'jordan.facibene13@stjohns.edu',
