7cb009bf1b65b824df1053017d0d25708832f2e6
Jordan Facibene <jordan.facibene13@stjohns.edu>
2016-06-20 13:13:41 -0400

    pypi issues, trying to fix

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'autostager',
   packages = ['autostager'],
-  version = '0.1.24',
+  version = '0.1.31',
   description = 'Stage a directory based on Github pull request (e.g., dynamic puppet environments)',
   author = 'Jordan Facibene',
   author_email = 'jordan.facibene13@stjohns.edu',
   url = 'https://github.com/jfach/autostager',
-  download_url = 'https://github.com/jfach/autostager/tarball/0.1.3',
+  download_url = 'https://github.com/jfach/autostager/tarball/0.1.31',
   keywords = ['github', 'automation', 'staging'],
   classifiers = [],
   install_requires = ['github3.py']
